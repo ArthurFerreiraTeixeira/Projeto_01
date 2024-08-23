2fee97ebc582865d604b46862209344c5f7e22b3
Arthur Ferreira <arthur203teixeira@gmail.com>
2024-08-23 18:20:22 -0300

    novo

---
+++ script.js
@@ -0,0 +1,133 @@
+//function imprimir(){
+   // document.write ('<p>Contador<p>')
+//}
+
+//let contador = document.getElementById('contador')
+
+//var timer = setInterval (()=> {
+  // contador.innerHTML ++ 'Hello!<br/>'
+//}, 1000)
+
+//function stopTimer(){
+  // clearInterval(timer)
+//}
+
+//setTimeout(imprimir,3000)
+
+//setTimeout(() => {
+    //contador.innerHTML += 'Hello!<br/>'
+//}, 2000)
+
+//var nome= 'Arthur'
+
+//if(nome === 'Arthur'){
+   // let cargo = 'CEO'
+    //cargo = 'Manager'
+    //console.log(cargo)
+//}
+
+//if(nome === 'Arthur'){
+    //const status = 'sucess'
+    // status = 'erro'
+    //console.log(status)
+//}
+
+//let pessoa = {
+    //nome: 'Arthur',
+    //idade: 32,
+    //cargo: 'Fullstack'
+//}
+
+//let users = [
+//{nome: 'Arthur', idade 23, cargo:'CEO'},
+//{nome: 'Fabio', idade 20, cargo:'Fullstack'},
+//{nome: 'Gustavo', idade 19, cargo:'Estagiario'},
+//]
+
+//for (var i = 0; i < users.length; i++){
+    //console.log(users[i].nome)
+//}
+
+//let pessoa = {
+    //nome: 'Arthur',
+    //idade: 21,
+    //profissao: 'Desenvolvedor Fullstack',
+//}
+
+//let (nome, idade) = pessoa
+//console.log(nome)
+
+//let numerosPares = [2,4,6,8.10]
+//let numerosImpares = [...numerosPares,1,3,5,7,9]
+
+//console.log(numerosImpares)
+
+//let pessoa = {
+    //nome:'Arthur',
+    //idade: 21,
+//}
+
+//let profissional = {
+    //...pessoa,
+    //cargo: 'DEV',
+//}
+
+//console.log(profissional)
+
+//function user(info) {
+    //let data = {
+       // ...info,
+        //stats: 'ok',
+        //inicio:'2024',
+    //}
+    //console.log(data)
+//}
+
+//user ({nome:'Arthur', email:'arthur2003teixeira@gmail.com' })
+
+//function gerarNumeros (...numeros){
+    //console.log(numeros)
+    //const numeroSorteado =Math.floor(Math.random()*numeros.length)
+    //console.log(numeros[numeroSorteado])
+//}
+
+//gerarNumeros(2, 4, 5, 67, 43, 12, 78)
+
+//let nomes = ['Arthur', 'Fabio', 'Gustavo', 'Guntzel']
+
+//nomes.map(('item' + index+ 'items' + item)(
+//))
+
+/*
+() => {}
+parenteses
+seta =>
+    chames
+*/
+
+//function somaRegular(a,b){
+    //let total = a + b
+    //return console.log(total)
+//}
+
+//let soma = (b, c) => {
+    //let result = b + c
+    //console.log(result) 
+///}
+
+//somaRegular(10, 10)
+//soma(20, 20)
+
+//'https://regres.in/api/users?page=2'
+//let users ={}
+
+//function getUsers(){
+    //fetch ('https://regres.in/api/users?page=2')
+    //.then ((response) => {
+        //return response.json()
+    //})
+    //.then ((data) => {
+        //console.log(data)
+    //})
+//}
+//getUsers()
